e88ef073adbe654617d9d04a6a6cb9d34c2366d8
3vincent <bsukstorf@gmail.com>
2022-01-22 15:36:28 +0100

    Fix indentation

--- nextcloud-backup.sh
+++ nextcloud-backup.sh
@@ -14,11 +14,11 @@ TMP_PATH=/tmp/
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
-#	1. Activate Maintenance Mode
-#	2. Backup Installation Dir in Apache Web Folder
-#	3. Backup Data Dir
+# 1. Activate Maintenance Mode
+# 2. Backup Installation Dir in Apache Web Folder
+# 3. Backup Data Dir
 # 4. Backup MySQL Database
-#	5. Deactivate Maintenance Mode
+# 5. Deactivate Maintenance Mode
 # 6. Size, Location and Info-Output
 #
 #    Source mainly: https://www.c-rieger.de/nextcloud-sicherung-und-wiederherstellung/
@@ -34,11 +34,13 @@ TMP_PATH=/tmp/
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 function nextcloudMaintananceModeOn {
-	sudo -u $apacheUser $nextcloudInstallation/occ maintenance:mode --on >/dev/null
+  echo "Turn Nextcloud Maintenance Mode ON"
+  sudo -u $apacheUser $nextcloudInstallation/occ maintenance:mode --on >/dev/null
 }
 
 function nextcloudMaintananceModeOff {
-	sudo -u $apacheUser $nextcloudInstallation/occ maintenance:mode --off >/dev/null
+  echo "Turn Nextcloud Maintenance Mode OFF"
+  sudo -u $apacheUser $nextcloudInstallation/occ maintenance:mode --off >/dev/null
 }
 
 
@@ -60,15 +62,15 @@ CLI_TOOLS=(
   "pv"
   "tar"
   "gzip"
-	"du"
+  "du"
 )
 
 for tool in ${CLI_TOOLS[@]}
 do
-	if [ ! $(which $tool) ]; then
-		echo "***error *** $tool does not exist on this system. Please install it! Exiting..."
+  if [ ! $(which $tool) ]; then
+    echo "***error *** $tool does not exist on this system. Please install it! Exiting..."
     exit
-	fi
+  fi
 done
 
 
@@ -80,8 +82,8 @@ DATESTAMP=$(date +%Y-%m-%d)
 backupDestination="$backupDestination/nextcloud_backup_$DATESTAMP"
 
 if [ -d $backupDestination ]; then
-	echo "*** error*** Backup Location: $backupDestination already exists!"
-	exit
+  echo "*** error*** Backup Location: $backupDestination already exists!"
+  exit
 fi
 
 mkdir $backupDestination
@@ -89,18 +91,18 @@ mkdir $backupDestination
 echo "############## Nextcloud Backup 101 ##############"
 
 
-###	1. Activate Maintenance Mode in nextcloud
+### 1. Activate Maintenance Mode in nextcloud
 ###
 
 if (nextcloudMaintananceModeOn); then
-	echo "1. Turn Nextcloud Maintenance Mode ON"
+  echo "..okay"
 else
-	echo "***error *** Nextcloud occ Maintenance Mode was not successfull!"
-	exit
+  echo "***error *** Nextcloud occ Maintenance Mode was not successfull!"
+  exit
 fi
 
 
-###	2. Backup installation directories and files and move to backupDestination
+### 2. Backup installation directories and files and move to backupDestination
 ###
 
 # set default size to zero for counting the 
@@ -108,52 +110,54 @@ fi
 sizeOfDir=0 
 
 if [ ! -d "$backupDestination" ]; then
-	echo "***error *** Directory not found: $backupDestination"
-	nextcloudMaintananceModeOff
-	exit 1
+  echo "***error *** Directory not found: $backupDestination"
+  nextcloudMaintananceModeOff
+  exit 1
+fi
 
 if [ ! -d "$nextcloudInstallation" ]; then
-	echo "***error *** Directory not found: $nextcloudInstallation"
+  echo "***error *** Directory not found: $nextcloudInstallation"
   nextcloudMaintananceModeOff
-	exit 1
+  exit 1
 fi
 
 if [ -d "$backupDestination" ] && [ -d "$nextcloudInstallation" ]; then
-	echo "2. Creating Backup of Installation Directory $nextcloudInstallation ..."
-	sizeOfDir=$(du -sk "$nextcloudInstallation" | cut -f 1)
-	tar -cpf - -C "$nextcloudInstallation" . | pv --size ${sizeOfDir}k -p --timer --rate --bytes | gzip -c > "$backupDestination/nextcloud-InstallationDir_$DATESTAMP.tar.gz"
+  echo "2. Creating Backup of Installation Directory $nextcloudInstallation ..."
+  sizeOfDir=$(du -sk "$nextcloudInstallation" | cut -f 1)
+  tar -cpf - -C "$nextcloudInstallation" . | pv --size ${sizeOfDir}k -p --timer --rate --bytes | gzip -c > "$backupDestination/nextcloud-InstallationDir_$DATESTAMP.tar.gz"
+fi
 
 echo ""
 
 
-###	3. Backup Data Directory
+### 3. Backup Data Directory
 ###
 
 if [ ! -d "$backupDestination" ]; then
-        echo "***error *** Directory not found: $backupDestination"
-				nextcloudMaintananceModeOff
-        exit 1
+  echo "***error *** Directory not found: $backupDestination"
+  nextcloudMaintananceModeOff
+  exit 1
 fi
 
 if [ ! -d "$nextcloudInstallation" ]; then
-        echo "***error *** Directory not found: $nextcloudInstallation"
-				nextcloudMaintananceModeOff
-        exit 1
+  echo "***error *** Directory not found: $nextcloudInstallation"
+  nextcloudMaintananceModeOff
+  exit 1
 fi
 
 if [ -d "$backupDestination" ] && [ -d "$nextcloudData" ]; then
-        echo "3. Creating Backup of Data Directory $nextcloudData ..."
-	sizeOfDir=$(du -sk "$nextcloudData" | cut -f 1)
-        tar -cpf - -C "$nextcloudData" . | pv --size ${sizeOfDir}k -p --timer --rate --bytes | gzip -c > "$backupDestination/nextcloud-DataDir_$DATESTAMP.tar.gz"
+  echo "3. Creating Backup of Data Directory $nextcloudData ..."
+  sizeOfDir=$(du -sk "$nextcloudData" | cut -f 1)
+  tar -cpf - -C "$nextcloudData" . | pv --size ${sizeOfDir}k -p --timer --rate --bytes | gzip -c > "$backupDestination/nextcloud-DataDir_$DATESTAMP.tar.gz"
 fi
 
-###	4. MySql Backup
+### 4. MySql Backup
 ###
 
 if [ ! -d $backupDestination ]; then
-	echo "***error *** Directory does not exist: $backupDestination"
-	nextcloudMaintananceModeOff
-	exit 1
+  echo "***error *** Directory does not exist: $backupDestination"
+  nextcloudMaintananceModeOff
+  exit 1
 fi
 
 echo "4. Creating Backup of MySQL Database $mysqlDatabase ..."
@@ -163,17 +167,17 @@ gzip < ${TMP_PATH}/nextcloud_db_backup_tempfile_${DATESTAMP}.sql > "$backupDesti
 rm ${TMP_PATH}/nextcloud_db_backup_tempfile_${DATESTAMP}.sql
 
 
-###	5. Deactivate Maintenance Mode
+### 5. Deactivate Maintenance Mode
 ###
 
 if (nextcloudMaintananceModeOff); then
-	echo "5. Nextcloud Maintenance Mode OFF"
+  echo "...okay"
 else
-	echo "***error *** Something went wrong with turning nextcloud maintenance mode off"
+  echo "***error *** Something went wrong with turning nextcloud maintenance mode off"
 fi
 
 
-###	6. Size, Location, Infomation Output
+### 6. Size, Location, Infomation Output
 ###
 
 backupSize=$(du -csh $backupDestination | grep total | awk '{ print $1 }')
